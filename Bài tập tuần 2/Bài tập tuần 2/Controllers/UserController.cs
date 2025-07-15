1a3336c577f6cef17e56ea17529078efd80dd076
Nhanokk1 <dangnhan020@gmail.com>
2025-07-16 00:06:58 +0700

    tuan2

---
+++ Bài tập tuần 2/Bài tập tuần 2/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Bài_tập_tuần_2.Models;
+using Bài_tập_tuần_2.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bài_tập_tuần_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserReposiory _repo;
+
+        public UserController(IUserReposiory repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _repo.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _repo.GetByIdAsync(id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            await _repo.AddAsync(user);
+            return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, User user)
+        {
+            if (id != user.Id) return BadRequest();
+            await _repo.UpdateAsync(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repo.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Bài tập tuần 2/Bài tập tuần 2/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Bài_tập_tuần_2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public List<TaskItem> Tasks { get; set; } = new();
+    }
+}
