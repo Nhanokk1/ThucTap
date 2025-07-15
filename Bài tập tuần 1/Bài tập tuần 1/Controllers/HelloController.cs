c8f26a4896e1e88d2247e954b9d96ce8c7ee9ba1
Nhanokk1 <dangnhan020@gmail.com>
2025-07-15 23:27:15 +0700

    tuan1

---
+++ Bài tập tuần 1/Bài tập tuần 1/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bài_tập_tuần_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ Bài tập tuần 1/Bài tập tuần 1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Bài_tập_tuần_1
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
